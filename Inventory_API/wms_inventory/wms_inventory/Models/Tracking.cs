1e2460133780ebaca16a33e8abe3fa016d01a919
BburnN123 <huijie_1995@hotmail.com>
2020-10-24 01:23:14 +0800

    React JS setting done

--- Inventory_API/wms_inventory/wms_inventory/Models/Tracking.cs
+++ Inventory_API/wms_inventory/wms_inventory/Models/Tracking.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,18 @@ namespace wms_inventory.Models
 {
     public class Tracking
     {
+        [Key]
         public int id { get; set; }
         [Required]
+        [Column(TypeName ="nvarchar(500)")]
         public string trackingId { get; set; }
+        [Column(TypeName = "nvarchar(500)")]
         public string company { get; set; }
+        [Column(TypeName = "int")]
         public string packingSize { get; set; }
+        [Column(TypeName = "datetime")]
         public DateTime datetime { get; set; }
+        [Column(TypeName = "int")]
         public int packing_weight { get; set; }
     }
 }
--- Inventory_API/wms_inventory/wms_react/Startup.cs
+++ Inventory_API/wms_inventory/wms_react/Startup.cs
@@ -1,40 +1,27 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using wms_inventory.Models;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 
-namespace wms_inventory
+namespace wms_react
 {
     public class Startup
     {
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        private IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
 
-            // Instead of creating a new instance, use AppDbContext
-            // Performance is better
-            // It keeps multiple DbContext objects alive and gives you an unused one rather than creating a new one each time.
-            services.AddDbContextPool<AppDbContext>(
-                options => options.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
-
-            // A new instance is provided to every controller and every service.
-            // Provide which repository you want to use Mock or SQL
-            // SQL => AddScoped
-            services.AddScoped<ITrackingRepository, MockTrackingRepository>();
-
+            services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -50,24 +37,26 @@ namespace wms_inventory
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
-
-            app.UseDefaultFiles();
-
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
 
-            // Using react to start
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = "ClientApp";
