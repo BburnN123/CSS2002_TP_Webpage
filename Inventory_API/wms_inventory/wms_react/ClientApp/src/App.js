5dea3d75adf83f738ace20294adff04e7626d06a
BburnN123 <huijie_1995@hotmail.com>
2020-10-24 11:01:08 +0800

    Dashboard install successfully

--- Inventory_API/wms_inventory/wms_react/ClientApp/src/App.js
+++ Inventory_API/wms_inventory/wms_react/ClientApp/src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from 'src/components/GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
 
-import './custom.css'
+const App = () => {
+  const routing = useRoutes(routes);
 
-export default class App extends Component {
-  static displayName = App.name;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {routing}
+    </ThemeProvider>
+  );
+};
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
-}
+export default App;
